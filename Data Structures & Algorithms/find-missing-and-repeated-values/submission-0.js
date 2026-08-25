f6469c370e1564d4eb309f03ae808f4f44a51b5e
Scott Au Yeung <auyeungscott@gmail.com>
2026-08-25 13:27:47 -0400

    Add: find-missing-and-repeated-values - submission-0

---
+++ Data Structures & Algorithms/find-missing-and-repeated-values/submission-0.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number[]}
+     */
+    findMissingAndRepeatedValues(grid) {
+        let map = new Map();
+        let count = 0;
+        let answer = [];
+        //loops through the array of arrays
+        for(const array of grid){
+            //loops through the index of arrays
+            for(const index of array){
+                if(map.has(index)){
+                    answer.push(index);
+                    count++;
+                }else{
+                    map.set(index, 1);
+                    count++;
+                }
+            }
+        }
+        console.log(count)
+        for(let i = 1;i<count+1;i++){
+            if(!map.has(i)){
+                answer.push(i)
+            }
+        }
+
+        return answer;
+    }
+}
