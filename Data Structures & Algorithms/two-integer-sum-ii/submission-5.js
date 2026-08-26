e263e873a3f82ecd0850ce50db0bb010717bd500
Scott Au Yeung <auyeungscott@gmail.com>
2026-08-26 08:35:41 -0400

    Add: two-integer-sum-ii - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-5.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        let first = 0
+        let last = numbers.length - 1
+
+        while(first < last){
+            if(numbers[first] + numbers[last] === target){
+                return [first + 1, last + 1];
+            }
+
+            if(numbers[first] + numbers[last] < target){
+                first ++;
+            }else{
+                last--;
+            }
+        }
+    }
+}
